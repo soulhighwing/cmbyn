d31cbf7b1825cc67e9be076214f727393fc088cb
soulhighwing <soulhighwing@gmail.com>
2018-04-02 01:25:03 +0800

    Add contact list for get contacts and will modify for save contacts

--- cmbyn/ContactsViewController.m
+++ cmbyn/ContactsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ContactsViewController.h"
+#import "ContactList.h"
 
 @interface ContactsViewController ()
 
@@ -17,11 +18,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-}
+    [[ContactList sharedContacts] fetchAllContacts]; // fetch all contacts by calling single to method
+    
+    if ([[ContactList sharedContacts]totalPhoneNumberArray].count !=0) {
+        NSLog(@"Fetched Contact Details : %ld",[[ContactList sharedContacts]totalPhoneNumberArray].count);
+    }
+  }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+#pragma mark - contacts access
+
+#pragma mark - Table view
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"ContactCell";
+    
+    UITableViewCell *cell =
+    [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    cell.textLabel.text = [NSString stringWithFormat:@"contact name"];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"123 %@ ",cell.detailTextLabel.text];
+
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return NO;
 }
 
 /*
